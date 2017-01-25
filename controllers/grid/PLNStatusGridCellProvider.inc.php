e22c86372c9b25ff3d77dce2673866a4f1994f80
Dimitris Efstathiou <defstat@gmail.com>
2017-01-25 11:27:23 +0200

    fix defstat/PKP-PLN#3 Remove dead (commented) code

--- controllers/grid/PLNStatusGridCellProvider.inc.php
+++ controllers/grid/PLNStatusGridCellProvider.inc.php
@@ -24,35 +24,6 @@ class PLNStatusGridCellProvider extends GridCellProvider {
 		parent::__construct();
 	}
 
-
-	//
-	// Template methods from GridCellProvider
-	//
-	///**
-	// * Get cell actions associated with this row/column combination
-	// * @param $row GridRow
-	// * @param $column GridColumn
-	// * @return array an array of LinkAction instances
-	// */
-	//function getCellActions($request, $row, $column, $position = GRID_ACTION_POSITION_DEFAULT) {
-	//    $staticPage = $row->getData();
-
-	//    switch ($column->getId()) {
-	//        case 'path':
-	//            $dispatcher = $request->getDispatcher();
-	//            return array(new LinkAction(
-	//                'details',
-	//                new RedirectAction(
-	//                    $dispatcher->url($request, ROUTE_PAGE, null) . '/' . $staticPage->getComplete(),
-	//                    'staticPage'
-	//                ),
-	//                $staticPage->getComplete()
-	//            ));
-	//        default:
-	//            return parent::getCellActions($request, $row, $column, $position);
-	//    }
-	//}
-
 	/**
 	 * Extracts variables for a given column from a data element
 	 * so that they may be assigned to template before rendering.
