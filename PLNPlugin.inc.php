86e177b881e2899b73f25f25b1519e469ab33009
Alec Smecher <alec@smecher.bc.ca>
2019-06-20 08:47:44 -0700

    Fix incorrect constant name

--- PLNPlugin.inc.php
+++ PLNPlugin.inc.php
@@ -338,7 +338,7 @@ class PLNPlugin extends GenericPlugin {
 				if (in_array($op, array('deposits'))) {
 					define('HANDLER_CLASS', 'PLNHandler');
 					define('PLN_PLUGIN_NAME', $this->getName());
-					AppLocale::requireComponents(LOCALE_COMPONENT_APPLICATION_COMMON);
+					AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
 					$handlerFile =& $args[2];
 					$handlerFile = $this->getHandlerPath() . '/' . 'PLNHandler.inc.php';
 				}
